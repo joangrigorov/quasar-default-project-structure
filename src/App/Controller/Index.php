aa2f20e05f7511cd0ca2bfd9bc04076d4556b3bf
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-09-16 22:51:34 +0300

    Added default index namespace

--- src/App/Controller/Index.php
+++ src/App/Controller/Index.php
@@ -5,6 +5,12 @@ use Quasar\Controller\Controller;
 
 class Index extends Controller
 {
+    
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+    
     public function execute()
     {
     }
